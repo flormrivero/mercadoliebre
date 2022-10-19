e75dc2c6521060355edf6ee3ac4de2b1c00c4445
Florencia Rivero <113300135+flormrivero@users.noreply.github.com>
2022-10-19 13:19:58 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static(publicFolder));
 
-const app_port=1234;
-app.listen(app_port, () => {
-    console.log('Servidor funcionando')
-})
+const port= process.env.PORT || 3001;
+app.listen(port, () => console.log('Servidor corriendo en el puerto ${port}'));
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("views/home.html"))
 })
-app.get("/registro", (req, res) => {
+app.get("/register", (req, res) => {
     res.sendFile(path.resolve("views/register.html"))
 })
 app.get("/login", (req, res) => {
